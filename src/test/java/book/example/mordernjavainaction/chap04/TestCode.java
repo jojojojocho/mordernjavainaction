cd5dd3f989a0b4f807fe076c9e64e84ea7d79b33
jo <43841476+jojojojocho@users.noreply.github.com>
2022-08-30 19:21:42 +0900

    chap05의 테스트 코드를 위하여 chap04의 TestCode 테스트 클래스에 전역변수로 선언이 되어 있던 menu를 Dish의 makeDishes메서드로 변경하였음. makeDishes를 호출하게 되면 List<Dish>가 반환 됨.

--- src/test/java/book/example/mordernjavainaction/chap04/TestCode.java
+++ src/test/java/book/example/mordernjavainaction/chap04/TestCode.java
@@ -15,17 +15,7 @@ import static java.util.stream.Collectors.*;
 public class TestCode {
 
 
-    List<Dish> menu = Arrays.asList(
-            new Dish("pork", false, 800, Dish.Type.MEAT),
-            new Dish("beef", false, 700, Dish.Type.MEAT),
-            new Dish("chicken", false, 400, Dish.Type.MEAT),
-            new Dish("french_fries", true, 530, Dish.Type.OTHER),
-            new Dish("rice", true, 350, Dish.Type.OTHER),
-            new Dish("season fruit", true, 120, Dish.Type.OTHER),
-            new Dish("pizza", true, 550, Dish.Type.OTHER),
-            new Dish("prawns", false, 300, Dish.Type.FISH),
-            new Dish("salmon", false, 450, Dish.Type.FISH)
-    );
+    List<Dish> menu = new Dish().makeDishes();
 
     @DisplayName("자바 7코드")
     @Test
